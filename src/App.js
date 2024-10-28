0a5a3016503236241706d9ad6c7d388e3dab1795
Simão Barbosa <simaopsbarbosa@gmail.com>
2024-10-28 17:44:28 +0000

    added main page

--- src/App.js
+++ src/App.js
@@ -2,7 +2,15 @@ import './App.css';
 
 function App() {
   return (
-    <h1>hello world!</h1>
+    <div class="home-box">
+      <h1>Simão Barbosa,</h1>
+      <h2>Informatics and Computing Engineering student at FEUP</h2>
+      <div className='shortcuts-div'>
+        <a>about</a>
+        <a>projects</a>
+        <a>contact</a>
+      </div>
+    </div>
   );
 }
 
